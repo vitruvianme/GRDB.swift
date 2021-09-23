b4ba264257eedc22c6b223e66e962e85538e19f3
Izaak Sultan <izaaksultan@gmail.com>
2021-09-23 19:25:51 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "GRDB",
     platforms: [
-        .iOS(v14),
-        .macOS(v11),
-        .tvOS(v13),
-        .watchOS(v7),
+        .iOS(.v14),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v7),
     ],
     products: [
         .library(name: "GRDB", targets: ["GRDB"]),
